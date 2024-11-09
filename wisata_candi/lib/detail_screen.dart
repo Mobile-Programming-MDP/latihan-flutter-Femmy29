6ce4ebde8499ea6ffa58b8918124952f2c587a46
Femmy29 <femmyjohan_2327250035@mhs.mdp.ac.id>
2024-11-09 12:59:53 +0700

    Search_screen candi

--- wisata_candi/lib/detail_screen.dart
+++ wisata_candi/lib/detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/candi.dart';
 
 class DetailScreen extends StatelessWidget {
   final Candi candi;
--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/detail_screen.dart';
-// import 'package:wisata_candi/models/profil_screen.dart';
+import 'package:wisata_candi/search_screen.dart';
+import 'package:wisata_candi/profil_screen.dart';
+import 'package:wisata_candi/profil_info_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DetailScreen(
-        candi: candiList[8],
-      ),
       // home: ProfilScreen(),
+      // home: DetailScreen(
+      //   candi: candiList[8],
+      // ),
+      home: SearchScreen(),
     );
   }
 }
